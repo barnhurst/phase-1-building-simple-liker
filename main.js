a18f1582fe16bab9e95e36531b6bc8550c3b2b72
Christopher <c.barnhurst@live.com>
2021-09-15 14:14:29 -0500

    commit tree 25f4084891ac4c8b1dd927570c22464bbe868218

--- main.js
+++ main.js
@@ -2,7 +2,45 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// query for elements with class like-glyph
+const heart = document.querySelectorAll('.like-glyph');
+//iterate over all heart elements
+heart.forEach(el => 
+  // added click event listener to elements
+  el.addEventListener('click', () => {
+  
+    mimicServerCall().then(() => {
+      like(el);
+    }).catch(() => {document.getElementById('modal').className ='visible';
+    
+    setTimeout(() => {
+      document.getElementById('modal').className ='hidden'
+    }, 3000);
+
+    })
+    
+
+
+  })
+  
+)
+function like(el){
+  console.log(el)
+  if (el.className === "like-glyph")
+  {
+    console.log("heart1")
+    el.innerHTML = FULL_HEART
+    el.className = "like-glyph activated-heart"
+  } 
+  else if (el.className === "like-glyph activated-heart") 
+  {
+    console.log("heart2")
+    el.innerHTML = EMPTY_HEART
+    el.className = "like-glyph"
+  }   
+}
+
+
 
 
 
